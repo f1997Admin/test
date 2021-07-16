3039b74d1808004aab46d96382a31c04558bf2fd
fj <192450101@qq.com>
2021-07-16 17:21:35 +0800

    123

---
+++ _02_mybatis01/src/main/java/com/abc/util/SqlSessionUtil.java
@@ -0,0 +1,37 @@
+package com.abc.util;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import javax.annotation.Resource;
+import java.io.InputStream;
+
+public class SqlSessionUtil {
+    private  static  SqlSessionFactory factory=null;
+    private SqlSessionUtil(){}
+    static {
+        String config="/mybatis.xml";
+        InputStream in= Resource.class.getResourceAsStream(config);
+        SqlSessionFactoryBuilder builder=new SqlSessionFactoryBuilder();
+        factory=builder.build(in);
+
+
+    }
+    private static  ThreadLocal<SqlSession> t=new ThreadLocal<>();
+    public static SqlSession getSession(){
+        SqlSession session=t.get();
+        if (session == null) {
+            session=factory.openSession();
+            t.set(session);
+        }
+        return session;
+    }
+    public static void myClose(SqlSession session){
+        if (session !=null) {
+            session.close();
+            t.remove();
+        }
+    }
+
+}
